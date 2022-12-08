5452be5a6422e5b28922ffaf5bf7ced01c51f01a
Gabriel Fuentes <gabrielfuentes7312@gmail.com>
2022-12-08 10:24:31 -0400

    wwise2

--- Assets/Scripts/Enemy/EnemyData.cs
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu([CreateAssetMenu(fileName = "New Tower Data", menuName = "Tower Data")])]
+[CreateAssetMenu(fileName = "New Enemy Data", menuName = "Enemy Data")]
 public class EnemyData : ScriptableObject
 {
     public float Life;
