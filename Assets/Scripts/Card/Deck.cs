39aa8a2ad672f5494b65f3feb884b6c803a364a9
Javier <1javier.ha@gmail.com>
2022-12-09 15:54:12 -0400

    Make cards, cards deck, dropped cards scripts and objects

---
+++ Assets/Scripts/Card/Deck.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public List<Card> deck = new List<Card>();
+    public Transform[] cardSlots;
+    public bool[] availableCardSlots;
+
+    public static Deck instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void SearchAviableSlots(int card)
+    {
+        for(int i = 0; i < availableCardSlots.Length; i++)
+        {
+            if(availableCardSlots[i] == true)
+            {
+                Card newCard = Instantiate(deck[card], cardSlots[i].position, transform.rotation);
+                newCard.handIndex = i; 
+                availableCardSlots[i] = false;
+                return;
+            }
+        }
+    }
+}
