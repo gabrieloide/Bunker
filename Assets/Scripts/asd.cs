7fdffae1b70a6ca368c740c02ed189c0a5b7b393
Gabriel Fuentes <gabrielfuentes7312@gmail.com>
2022-12-26 10:13:59 -0400

    agregando test para parabola 2

---
+++ Assets/Scripts/asd.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asd : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform objTrans;
+    public float h = 10;
+    public float gravity = -9.8f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
